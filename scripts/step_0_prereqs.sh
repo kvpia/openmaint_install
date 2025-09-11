2a4e9abf6af110cd9d95becbaa3ef33287dc6ff7
kvpia <yakimovetc@gmail.com>
2025-09-11 12:16:26 +0300

    Update step_0_prereqs.sh

--- scripts/step_0_prereqs.sh
+++ scripts/step_0_prereqs.sh
@@ -1,1 +1,17 @@
+# Цей скрипт готує систему для встановлення OpenMAINT та необхідних компонентів.
+# Він оновлює систему і встановлює базові пакети та залежності.
+
+# Оновлення списку пакетів
+echo "Оновлення списку пакетів..."
+sudo apt update -y
+
+# Оновлення встановлених пакетів
+echo "Оновлення встановлених пакетів..."
+sudo apt upgrade -y
+
+# Встановлення необхідних утиліт та бібліотек
+echo "Встановлення необхідних утиліт: wget, unzip, libxrender1, libcups2, fontconfig, libc6, libglib2.0-0, libsm6, libxext6, libxrender1, libxtst6"
+sudo apt install wget unzip git libxrender1 libcups2 fontconfig libc6 libglib2.0-0 libsm6 libxext6 libxrender1 libxtst6 -y
+
+echo "Підготовка системи завершена."
 
