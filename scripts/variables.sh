7ce7f904284a50f04334a42d73f5a17f4a0e15db
kvpia <yakimovetc@gmail.com>
2025-09-11 17:37:44 +0300

    Update variables.sh

--- scripts/variables.sh
+++ scripts/variables.sh
@@ -14,7 +14,8 @@ function print_header() {
 # Alfresco
 ALFRESCO_URL="https://master.dl.sourceforge.net/project/alfresco/Alfresco%20201707%20Community/alfresco-community-installer-201707-linux-x64.bin?viasf=1"
 ALFRESCO_FALLBACK_URL="https://sourceforge.net/projects/alfresco/files/Alfresco%20201707%20Community/alfresco-community-installer-201707-linux-x64.bin/download"
-ALFRESCO_INSTALL_DIR="/opt/alfresco"
+ALFRESCO_INSTALL_DIR="/opt/alfresco-community"
+ALFRESCO_USER="alfresco"
 
 # PostgreSQL
 POSTGRES_PASSWORD="postgres"
@@ -27,16 +28,4 @@ GEOSERVER_USER="geoserver_user"
 
 # OpenMAINT
 OPENMAINT_VERSION="4.0.4"
-OPENMAINT_URL="https://downloads.sourceforge.net/project/openmaint/2.4/openmaint-2.4-$OPENMAINT_VERSION.war"
-OPENMAINT_INSTALL_DIR="/opt/openmaint"
-OPENMAINT_USER="openmaint_user"
-
-# BIMserver
-TOMCAT_VERSION="9.0.109"
-TOMCAT_URL="https://dlcdn.apache.org/tomcat/tomcat-9/v$TOMCAT_VERSION/bin/apache-tomcat-$TOMCAT_VERSION.tar.gz"
-BIMSERVER_VERSION="1.5.188"
-BIMSERVER_TAG_VERSION="1.5.188"
-BIMSERVER_WAR_URL="https://github.com/opensourceBIM/BIMserver/releases/download/v$BIMSERVER_TAG_VERSION/bimserverwar-$BIMSERVER_VERSION.war"
-BIMSERVER_INSTALL_DIR="/opt/bimserver"
-BIMSERVER_USER="bimserver_user"
-
+OPENMAINT_URL="https://downloads.sourceforge.net/project/openmaint/2.4/openmaint-4.0.4.zip"
