cb59d03fcc619be17428538554105d53421c6a98
kvpia <yakimovetc@gmail.com>
2025-09-11 17:35:25 +0300

    Update run_all.sh

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -9,20 +9,13 @@
 # Отримання директорії, де знаходиться скрипт
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
-# Підключення змінних та функцій
-source "$SCRIPT_DIR/variables.sh"
-
-print_header "Послідовний запуск скриптів встановлення OpenMaint..."
-
-START_STEP=${1:-0} # За замовчуванням починаємо з кроку 0, якщо не вказано інше
-
 # Виконання кроків
 execute_step() {
     local step_num=$1
     local script_name=$2
     if [ "$START_STEP" -le "$step_num" ]; then
         echo -e "\n\033[1;33m--- Виконання $script_name ---\033[0m"
-        # Передача поточної директорії скрипта
+        # Виконання скрипта за його повним шляхом
         "$SCRIPT_DIR/$script_name"
         if [ $? -ne 0 ]; then
             echo "Помилка на кроці $step_num. Вихід."
@@ -32,6 +25,13 @@ execute_step() {
         echo "Пропуск кроку $step_num: $script_name"
     fi
 }
+
+# Підключення змінних та функцій
+source "$SCRIPT_DIR/variables.sh"
+
+print_header "Послідовний запуск скриптів встановлення OpenMaint..."
+
+START_STEP=${1:-0} # За замовчуванням починаємо з кроку 0, якщо не вказано інше
 
 execute_step 0 "step_0_prereqs.sh"
 execute_step 1 "step_1_install_java17.sh"
